b07650fab9ae0a4d2bd0cc21168b28ae1beed293
vvsh <vvsh1978@mail.ru>
2020-05-04 23:11:00 +0500

    add gulp-cssmin in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -9,4 +11,12 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('css-min', function (done) {
+    gulp.src('./**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+        done();
 });
