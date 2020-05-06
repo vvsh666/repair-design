f160011b2d7898d0d5957cf82fe1e7f43e125fd1
vvsh <vvsh1978@mail.ru>
2020-05-06 16:32:18 +0500

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,35 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
+// const cssmin = require('gulp-cssmin');
+// const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('css-min', function (done) {
-    gulp.src(['css/*.css', '!css/*.min.css'])
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'));
-        done();
-});
+// gulp.task('css-min', function (done) {
+//     gulp.src(['css/*.css', '!css/*.min.css'])
+//         .pipe(cssmin())
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(gulp.dest('css'));
+//         done();
+// });
+
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serv = bs;
