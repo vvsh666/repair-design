d8914b99d6ff224991e3cdc3e3f3416cee447317
shilyaev <vvsh1978@mail.ru>
2020-05-13 23:55:01 +0500

    feat: add close modal with Esc keydown

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]')
-  const closeBtn = document.querySelector('.modal__close')
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
-    
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.code == 'Escape') {
+      if (modal.classList.contains('modal--visible')) {
+        switchModal();
+      }
+    }
   });
 
   closeBtn.addEventListener('click', switchModal);
 
+  
+
+
+
+
 });
+
