57fb3935127e873cd7350f5a9441a24ad25e8753
vvsh <vvsh1978@mail.ru>
2020-05-14 15:21:51 +0500

    refact: rewrite main.js on jQuery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/* document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -25,6 +25,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+
+}); */
+
+$(document).ready(function(){
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible')
+  });
+
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible')
+  });
+
+  $(document).on('keydown', function() {
+    modal.removeClass('modal--visible')
+  });
+
+  $(document).on('click', function(event) {
+    if ($(event.target).is('.modal--visible')) {
+      modal.toggleClass('modal--visible')
+    }
+  });
+
 
 });
 
