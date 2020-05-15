b32c97fdcd2500adf48778254394d2a3a36cf431
shilyaev <vvsh1978@mail.ru>
2020-05-16 02:53:31 +0500

    feat: add projects section with slaider

--- js/main.js
+++ js/main.js
@@ -68,6 +68,26 @@ $(document).ready(function(){
     return false;
   });
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 20 + bullets.width() + 20);
+  bullets.css('left', prev.width() + 20)
+
+
 
 });
 
