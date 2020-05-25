fb5371e3918394c3706bb8eba2a48e353d5d2575
svv <vvsh1978@mail.ru>
2020-05-25 18:27:04 +0500

    feat: install & add clean-css, minify, htmlmin in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cssmin = require('gulp-cssmin');
-const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 function bs() {
     serveSass();
-    // serveSccMin();
     browserSync.init({
         server: {
             baseDir: "./"
@@ -20,21 +20,44 @@ function bs() {
     watch("./js/*.js").on('change', browserSync.reload);
 };
 
-/* function serveSccMin() {
-        src(['css/*.css', '!css/*.min.css'])
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(dest('css'));
-};
- */
 function serveSass() {
     return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
         .pipe(autoprefixer({
+            browsers: ['last 16 versions'],
             cascade: false
         }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
 
+function buildCSS(done) {
+    src(["css/**/**.css", "!css/**/**.min.css"])
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest("dist/css/"));
+    src("css/**/**.min.css").pipe(dest("dist/css/"));
+    done();
+};
+
+function buildJS(done) {
+    src(["js/**.js", "!js/**/**.min.js"])
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true,
+        }))
+        .pipe(dest("dist/js/"));
+    src("js/**.min.js").pipe(dest("dist/js/"));
+    done();
+};
+
+function buildHTML(done) {
+    src("*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest("dist/"));
+    done();
+}
+
 exports.serv = bs;
+exports.build = series(buildCSS, buildJS, buildHTML);
